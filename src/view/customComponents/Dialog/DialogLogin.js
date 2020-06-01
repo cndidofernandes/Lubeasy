d5c18931aa0422a0e4af05a1203aee0a525843e3
cndidofernades <candidofrnnds@gmail.com>
2020-06-02 00:38:08 +0100

    Make many changes

---
+++ src/view/customComponents/Dialog/DialogLogin.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import Dialog from '@material-ui/core/Dialog';
+import AppBar from '@material-ui/core/AppBar';
+
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import CloseIcon from '@material-ui/icons/Close';
+import Slide from '@material-ui/core/Slide';
+
+import LoginPage from "../pages/LoginPage";
+
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
+
+export default function DialogSignInParceiro({open, location, handleClose, onSucess}) {
+
+
+    return (
+        <div>
+            <Dialog fullScreen open={open} onClose={handleClose} TransitionComponent={Transition}>
+                <AppBar position={"relative"} style={{background: '#FFFFFF', boxShadow:'none'}}>
+                    <Toolbar>
+                        <IconButton edge="start" style={{color: 'black'}} onClick={handleClose} aria-label="close">
+                            <CloseIcon />
+                        </IconButton>
+                        <Typography variant="h6" style={{marginLeft: 16, flex: 1, color:'black'}}>
+                            Faça primeiro o login
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+
+                <LoginPage location={location} redirectUrl={'/evento'}/>
+
+            </Dialog>
+            
+        </div>
+    );
+}
+
+//index === 1 ? label+' ('+qtd+')' : label
